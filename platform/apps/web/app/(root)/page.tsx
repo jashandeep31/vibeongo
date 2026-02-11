c89d4283c92a03caebb72192fe53aa589a7fddde
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-11 22:39:32 +0530

    fix: shadcn styles working

---
+++ platform/apps/web/app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@repo/ui/components/button";
+
+export default function page() {
+  return (
+    <div>
+      <h1>This is waste of time page</h1>
+      <Button>Shadn cn is working </Button>
+    </div>
+  );
+}
