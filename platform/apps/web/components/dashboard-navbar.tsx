839174387fbb620b565c3ea5d2c67e83f3258339
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-11 23:09:24 +0530

    feat: folder structure of dashboard

---
+++ platform/apps/web/components/dashboard-navbar.tsx
@@ -0,0 +1,9 @@
+export default function DashboardNavbar() {
+  return (
+    <div className="border-b">
+      <div className="container py-3 ">
+        <h2 className="text-lg font-bold">VOG</h2>
+      </div>
+    </div>
+  );
+}
