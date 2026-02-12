f780efbc0e432cb069b324b2a5918f04b22abbf5
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-12 20:45:54 +0530

    feat: terminating instance function

---
+++ platform/apps/server/src/aws/services/terminate-ec2-instance.ts
@@ -0,0 +1,13 @@
+import { TerminateInstancesCommand } from "@aws-sdk/client-ec2";
+import { createAndGetEc2Client } from "../ec2-client.js";
+
+/**
+ * terminate the ec2 instance
+ */
+export const terminateEc2Instance = async (ids: string[]) => {
+  const command = new TerminateInstancesCommand({
+    InstanceIds: ids,
+  });
+  const client = createAndGetEc2Client("us-east-1");
+  return await client.send(command);
+};
--- platform/apps/server/src/test.ts
+++ platform/apps/server/src/test.ts
@@ -1,4 +1,3 @@
-import { RunInstancesCommandOutput } from "@aws-sdk/client-ec2";
 import { createEc2Instance } from "./aws/services/create-ec2-instance/index.js";
 import { db, ec2 } from "@repo/db";
 
@@ -6,6 +5,9 @@ export default async function test() {
   const ec2s = await db.select().from(ec2);
   if (ec2s.filter((ec) => ec.status === "running").length >= 1) {
     console.log(`already running ec2 look into billing i can't spin more `);
+    const ids: string[] = ec2s.map((instance) => instance.ec2_id);
+    // await terminateEc2Instance(ec2res.)
+    console.log(ids);
     return;
   }
   const ec2res = await createEc2Instance({
