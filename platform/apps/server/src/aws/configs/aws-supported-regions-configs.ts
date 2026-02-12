825d4e9f62affedc4512da8d8c9676598119837b
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-12 19:32:15 +0530

    feat: configs for the ec2 and folder resuture

---
+++ platform/apps/server/src/aws/configs/aws-supported-regions-configs.ts
@@ -0,0 +1,1 @@
+export const awsSupportedRegions = ["us-east-1"] as const;
---
+++ platform/apps/server/src/aws/configs/ec2-deployments-config.ts
@@ -0,0 +1,33 @@
+import { awsSupportedRegions } from "./aws-supported-regions-configs.js";
+
+//TODO:: make the ids automated using aws fetching current or same version for all the regions
+export const ec2DeploymentConfigs: {
+  region: (typeof awsSupportedRegions)[number];
+  awsLinuxImageId: string;
+}[] = [
+  {
+    region: "us-east-1",
+    awsLinuxImageId: "ami-0c1fe732b5494dc14",
+  },
+];
+//
+// export const getAWSLinuxAmis = async () => {
+//   console.log(`amis is running `);
+//   const command = new DescribeImagesCommand({
+//     Owners: ["amazon"],
+//     // Filters: [{ Name: "platform", Values: ["windows"] }],
+//     // Filters: [{ Name: "platform", Values: ["amazon"] }],
+//     //
+//     Filters: [
+//       { Name: "name", Values: ["amzn2-ami-hvm-*"] }, // Amazon Linux 2
+//       { Name: "state", Values: ["available"] },
+//       { Name: "architecture", Values: ["x86_64"] },
+//     ],
+//   });
+//   const client = createEc2Client("us-east-1");
+//   const res = await client.send(command);
+//
+//   console.log(res);
+// };
+//
+//
