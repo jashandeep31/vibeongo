70b308a785577be19c8c8d8ab727a6e9e5283b02
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-14 17:56:02 +0530

    feat: core is now go

---
+++ core/go.mod
@@ -0,0 +1,3 @@
+module github.com/jashandeep31/vibeongo/core
+
+go 1.25.6
---
+++ core/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Go server is running")
+	fmt.Println("Things are working ")
+
+}
