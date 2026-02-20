bef08fa2a95c92d0fa05a3fed9bc278bd158fb8a
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-20 22:07:16 +0530

    feat: go validation setup

--- core/cmd/bootstrap/main.go
+++ core/cmd/bootstrap/main.go
@@ -5,22 +5,42 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/go-playground/validator/v10"
 )
+
+var validate = validator.New()
+
+type TestSchema struct {
+	Name string `json:"name" validate:"required,min=4"`
+}
 
 func main() {
 	fmt.Println("Bootstrap script is running")
 
 	content, err := os.ReadFile("test.json")
 	if err != nil {
-		log.Fatalf("Failed to create the file test")
+		log.Fatalf("Failed to read file: %v", err)
 	}
 
-	var data map[string]any
+	fmt.Println(string(content))
+	var data TestSchema
 
+	// 1️⃣ Decode JSON into struct
 	err = json.Unmarshal(content, &data)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Invalid JSON: %v", err)
 	}
 
-	fmt.Println(data)
+	// 2️⃣ Validate struct
+	err = validate.Struct(data)
+	if err != nil {
+		fmt.Println("Validation errors:")
+		for _, e := range err.(validator.ValidationErrors) {
+			fmt.Printf("Field %s failed on %s\n", e.Field(), e.Tag())
+		}
+		return
+	}
+
+	fmt.Println("Valid JSON:", data)
 }
