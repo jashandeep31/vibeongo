d2babbac13070d5151e50fb29d89cd1a928c75f7
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-20 20:16:08 +0530

    feat: server setup

--- core/cmd/api/main.go
+++ core/cmd/api/main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/jashandeep31/vibeongo/core/cmd/internal/server"
 )
 
-var startTime = time.Now()
-
 func main() {
-	fmt.Println(startTime.Year())
-	server.Start(startTime)
+	server.Start()
 }
