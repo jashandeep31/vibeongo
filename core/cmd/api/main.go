457ce43350185eca462fd2d32a78f750a93d7512
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-21 11:32:33 +0530

    chore: refactor

--- core/cmd/api/main.go
+++ core/cmd/api/main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"github.com/jashandeep31/vibeongo/core/cmd/internal/server"
-)
+import "github.com/jashandeep31/vibeongo/core/internal/server"
 
 func main() {
 	server.Start()
