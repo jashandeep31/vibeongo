86dbf38018e8bff9c2123efd4cff242b6f0a7856
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-21 11:39:23 +0530

    feat: loading the json file

---
+++ core/cmd/bootstrap/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jashandeep31/vibeongo/core/internal/bootstrap"
+)
+
+func main() {
+	fmt.Println("Bootstrap is started")
+	bootstrap.Run()
+	// fucntio to process here
+	// fucntion to run commands
+}
--- core/internal/bootstrap/config.go
+++ core/internal/bootstrap/config.go
@@ -5,14 +5,10 @@ import (
 	"os"
 )
 
-func LoadConfig() ([]byte, error) {
-	// read the json file so that we can check waht we can with it
-	fmt.Println("Config is getting loaded")
-
-	file, err := os.ReadFile("config.json")
+func LoadConfig(filename string) ([]byte, error) {
+	file, err := os.ReadFile(filename)
 	if err != nil {
-		return nil, fmt.Errorf("Failed to load the config %w", err)
+		return nil, fmt.Errorf("Failed to load the config: %w", err)
 	}
-	fmt.Println(string(file))
 	return file, nil
 }
