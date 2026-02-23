4aef60c631bdba2b1727b3227f62ec3c3dc67c0a
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-23 22:27:05 +0530

    fix: setup and other script changes

--- test-scripts/multipass-setup.sh
+++ test-scripts/multipass-setup.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-VM_NAME="server" 
-USERNAME="ubuntu"
+VM_NAME="server"
+USERNAME="local"
 
-echo "Launching the server "
-
-# command is taked from official docs no version is hard coded yet
+echo "Launching the server..."
 multipass launch --name $VM_NAME
 
-echo "Creating user"
+echo "Creating user..."
 multipass exec $VM_NAME -- sudo useradd -m -s /bin/bash $USERNAME
 multipass exec $VM_NAME -- sudo usermod -aG sudo $USERNAME
 
-echo "Transferring files..."
-multipass transfer ./main $VM_NAME:/home/$USERNAME/
-multipass transfer ./config.json $VM_NAME:/home/$USERNAME/
-echo "Fixing permissions..."
-multipass exec $VM_NAME -- sudo chown $USERNAME:$USERNAME /home/$USERNAME/main
-multipass exec $VM_NAME -- sudo chmod +x /home/$USERNAME/main
+echo "Allow ubuntu user to write into /home/$USERNAME temporarily..."
+multipass exec $VM_NAME -- sudo chmod 777 /home/$USERNAME
 
-echo "Running binary..."
-multipass exec $VM_NAME -- sudo -u $USERNAME /home/$USERNAME/main
+echo "Transferring files directly to local home..."
+multipass transfer ./bootstrap $VM_NAME:/home/$USERNAME/bootstrap
+multipass transfer ./config.json $VM_NAME:/home/$USERNAME/config.json
+
+echo "Restoring proper ownership & permissions..."
+multipass exec $VM_NAME -- sudo chown -R $USERNAME:$USERNAME /home/$USERNAME
+multipass exec $VM_NAME -- sudo chmod 755 /home/$USERNAME
+multipass exec $VM_NAME -- sudo chmod +x /home/$USERNAME/bootstrap
+
+echo "Running bootstrap..."
+multipass exec $VM_NAME -- sudo -u $USERNAME /home/$USERNAME/bootstrap
 
 echo "Done."
