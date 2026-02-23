05f2c5fc5d0f81a5643dfda87360534dfafab239
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-23 17:23:53 +0530

    feat: run command

---
+++ core/internal/bootstrap/utils/run_command.go
@@ -0,0 +1,11 @@
+package utils
+
+import (
+	"os/exec"
+)
+
+func RunCommand(name string, args ...string) (string, error) {
+	cmd := exec.Command(name, args...)
+	out, err := cmd.CombinedOutput()
+	return string(out), err
+}
