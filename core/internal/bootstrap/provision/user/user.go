92af11c850847f6f8229db3ef335d68c7c97fcde
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-23 23:35:39 +0530

    feat: script fixes

--- core/internal/bootstrap/provision/user/user.go
+++ core/internal/bootstrap/provision/user/user.go
@@ -2,14 +2,33 @@ package user
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/jashandeep31/vibeongo/core/internal/bootstrap/utils"
 	"github.com/jashandeep31/vibeongo/core/internal/config"
 )
 
+func checkRootPrivileges() error {
+	if os.Getuid() != 0 {
+		return fmt.Errorf("this script must be run as root or with sudo (current UID: %d)", os.Getuid())
+	}
+	return nil
+}
+
 func CreateUser(systemUser config.SystemUser) error {
-	// create user
-	_, err := utils.RunCommand(
+	// Check for root/sudo before doing anything
+	if err := checkRootPrivileges(); err != nil {
+		return err
+	}
+
+	// Check if user already exists to avoid duplicate error
+	_, err := utils.RunCommand("id", "-u", systemUser.Username)
+	if err == nil {
+		return fmt.Errorf("user %s already exists", systemUser.Username)
+	}
+
+	// Create user
+	_, err = utils.RunCommand(
 		"useradd",
 		"-m",
 		"-s",
@@ -20,7 +39,7 @@ func CreateUser(systemUser config.SystemUser) error {
 		return fmt.Errorf("failed to create user: %w", err)
 	}
 
-	// add sudo if needed
+	// Add sudo if needed
 	if systemUser.IsSudoUser {
 		_, err := utils.RunCommand(
 			"usermod",
