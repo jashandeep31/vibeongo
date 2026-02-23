263e751746596546a1ff58cac22183b1f710995b
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-23 23:57:34 +0530

    feat: docker installer

--- core/internal/bootstrap/provision/docker/docker.go
+++ core/internal/bootstrap/provision/docker/docker.go
@@ -1,29 +1,19 @@
 package docker
 
 import (
-	"bytes"
 	"fmt"
-	"os/exec"
 
 	"github.com/jashandeep31/vibeongo/core/internal/config"
 )
 
-func Installer(pkg config.Package) {
-	fmt.Println("We are installing the docker")
+func Installer(pkg config.Package) error {
+	pkgName := pkg.Name
+	pkgConfig := pkg.Config
+	fmt.Println(pkgName, pkgConfig)
 
-	// running the commands to the terminal
-
-	cmd := exec.Command("docker", "-v")
-
-	var out bytes.Buffer
-	var stderr bytes.Buffer
-
-	cmd.Stdout = &out
-	cmd.Stderr = &stderr
-
-	err := cmd.Run()
+	err := installDocker()
 	if err != nil {
-		fmt.Println("err", err)
+		return err
 	}
-	fmt.Println(out.String())
+	return nil
 }
