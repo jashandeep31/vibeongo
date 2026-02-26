a18ea732798eff780930771051a4d61b0a855760
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-26 16:24:39 +0530

    feat: temp docker file

--- platform/apps/server/src/routes/test-routes.ts
+++ platform/apps/server/src/routes/test-routes.ts
@@ -7,4 +7,29 @@ routes.route("/test").get((req: Request, res: Response) => {
   });
 });
 
+routes
+  .route("/postgres-docker-compose-file")
+  .get((req: Request, res: Response) => {
+    const fileData = `services:
+  db:
+    image: postgres:18
+    restart: always
+    volumes:
+      - pgdata:/var/lib/postgresql    # <-- FIXED (no /data)
+    ports:
+      - "5432:5432"
+    environment:
+      POSTGRES_DB: default
+      POSTGRES_USER: postgres
+      POSTGRES_PASSWORD: "5"
+
+volumes:
+  pgdata:`;
+    res.status(200).json({
+      message: "postgres docker file is here",
+      data: {
+        file: fileData,
+      },
+    });
+  });
 export const testRoutes = routes;
