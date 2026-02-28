63dae4e873570ac9fc376f587cb9db24e79e0bb5
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-28 21:53:38 +0530

    feat: database

---
+++ platform/packages/database/src/schemas/environments.ts
@@ -0,0 +1,14 @@
+import { pgTable, uuid, text, timestamp } from "drizzle-orm/pg-core";
+import { users } from "./user.js";
+
+export const environments = pgTable("environmentsj", {
+  id: uuid().unique().defaultRandom().primaryKey(),
+  user_id: uuid()
+    .references(() => users.id)
+    .notNull(),
+
+  config: text().notNull(),
+
+  created_at: timestamp().defaultNow(),
+  updated_at: timestamp().defaultNow(),
+});
