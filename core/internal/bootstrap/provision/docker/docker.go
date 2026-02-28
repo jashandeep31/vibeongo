09c360043e9acb41d46156e561a9e23fa2bf9eef
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-28 22:10:39 +0530

    feat: creating docker compose files

--- core/internal/bootstrap/provision/docker/docker.go
+++ core/internal/bootstrap/provision/docker/docker.go
@@ -3,6 +3,8 @@ package docker
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"os"
 
 	"github.com/jashandeep31/vibeongo/core/internal/config"
 )
@@ -20,10 +22,21 @@ func Installer(pkg config.Package, systemUser config.SystemUser) error {
 type Container struct {
 	Name           string `json:"name"`
 	ComposeFileURL string `json:"compose_file_url"`
+	Filename       string `json:"filename"`
 }
 
 type DockerContainersConfig struct {
 	Containers []Container `json:"containers"`
+}
+
+// API response structs
+type fileData struct {
+	File string `json:"file"`
+}
+
+type aPIResponse struct {
+	Message string   `json:"message"`
+	Data    fileData `json:"data"`
 }
 
 func ScriptValidator(pkg config.Package) error {
@@ -35,6 +48,25 @@ func ScriptValidator(pkg config.Package) error {
 	for _, container := range cfg.Containers {
 		// clone the docker compose script
 		fmt.Println(container.ComposeFileURL)
+
+		// Getting the response from api
+		resp, err := http.Get("http://localhost:8000/postgres-docker-compose-file")
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+
+		var apiResp aPIResponse
+
+		// putting the values to the var
+		err = json.NewDecoder(resp.Body).Decode(&apiResp)
+		if err != nil {
+			return err
+		}
+
+		fileBytes := []byte(apiResp.Data.File)
+		os.WriteFile(container.Filename, fileBytes, 0644)
+
 	}
 	return nil
 }
