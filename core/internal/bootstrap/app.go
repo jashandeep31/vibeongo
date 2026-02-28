d8a3a48660063ab9d0443094e0018b05ab3312ff
Jashandeep Singh <jashandeep1659@gmail.com>
2026-02-28 22:40:56 +0530

    feat: docker run container

--- core/internal/bootstrap/app.go
+++ core/internal/bootstrap/app.go
@@ -9,52 +9,33 @@ import (
 )
 
 func Run() {
-	const runScript = false
-	// getting the config json file
 	file, err := LoadConfig("config.json")
 	if err != nil {
 		log.Fatalf("application startup failed: %v", err)
 	}
 
-	// validated validatedConfig
-	validatedConfig, err := ValidateConfig(file)
+	// getting config
+	config, err := ValidateConfig(file)
 	if err != nil {
 		log.Fatalf("config has error %v", err)
 	}
 
-	if runScript == true {
-		// creating the user
-		if err := user.CreateUser(validatedConfig.SystemUser); err != nil {
-			log.Fatalf("Failed to create user with error: %v", err)
-		}
-		for _, pkg := range validatedConfig.Packages {
-			switch pkg.Name {
-			case "docker":
-				// TODO: working with docker function
-				if err := docker.Installer(pkg, validatedConfig.SystemUser); err != nil {
-					log.Fatalf("docker install flow failed: %v", err)
-				}
-			default:
-				fmt.Printf("%s is missing\n", pkg.Name)
-			}
-		}
-	} else {
+	// creating the user
+	if err := user.CreateUser(config.SystemUser); err != nil {
+		log.Fatalf("Failed to create user with error: %v", err)
+	}
 
-		//
-		//
-		// Testing purposes things only
-		//
-		//
+	// running the configs
+	for _, pkg := range config.Packages {
+		switch pkg.Name {
 
-		fmt.Println("Only testing script is working")
-		for _, pkg := range validatedConfig.Packages {
-			switch pkg.Name {
-			case "docker":
-				if err := docker.ScriptValidator(pkg); err != nil {
-					log.Fatalf("docker validation failed: %v", err)
-				}
+		case "docker":
+			if err := docker.Installer(pkg, config.SystemUser); err != nil {
+				log.Fatalf("docker install flow failed: %v", err)
 			}
+
+		default:
+			fmt.Printf("%s is missing\n", pkg.Name)
 		}
-		// docker.ScriptValidator()
 	}
 }
--- core/internal/bootstrap/provision/docker/docker.go
+++ core/internal/bootstrap/provision/docker/docker.go
@@ -1,76 +1,20 @@
 package docker
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"os"
-
 	"github.com/jashandeep31/vibeongo/core/internal/config"
 )
 
 func Installer(pkg config.Package, systemUser config.SystemUser) error {
-	// err := installDocker(sysmtemUser)
-	// if err != nil {
-	// 	return err
-	// }
-
-	// run the config docker container
-	return nil
-}
-
-type Container struct {
-	Name           string `json:"name"`
-	ComposeFileURL string `json:"compose_file_url"`
-	Filename       string `json:"filename"`
-}
-
-type DockerContainersConfig struct {
-	Containers []Container `json:"containers"`
-}
-
-// API response structs
-type fileData struct {
-	File string `json:"file"`
-}
-
-type aPIResponse struct {
-	Message string   `json:"message"`
-	Data    fileData `json:"data"`
-}
-
-func ScriptValidator(pkg config.Package) error {
-	var cfg DockerContainersConfig
-	if err := json.Unmarshal(pkg.Config, &cfg); err != nil {
+	err := installDocker(systemUser)
+	if err != nil {
 		return err
 	}
 
-	for _, container := range cfg.Containers {
-		// clone the docker compose script
-		fmt.Println(container.ComposeFileURL)
-
-		// Getting the response from api
-		resp, err := http.Get("http://localhost:8000/postgres-docker-compose-file")
-		if err != nil {
-			return err
-		}
-		defer resp.Body.Close()
-
-		var apiResp aPIResponse
-
-		// putting the values to the var
-		err = json.NewDecoder(resp.Body).Decode(&apiResp)
-		if err != nil {
-			return err
-		}
-
-		// !!!! Why not folders  why? we are using direct files
-		// Sometimes the program is failing in the dev mode to create the files inside the folders
-		fileBytes := []byte(apiResp.Data.File)
-		os.WriteFile(container.Filename, fileBytes, 0644)
-
-		// utils.RunCommand("docker" , "compose" , "-f", container.Filename, "up", "-d")
-
+	// validating the script
+	if err := RunContainers(pkg); err != nil {
+		return err
 	}
+
+	// run the config docker container
 	return nil
 }
