a7d26b99b33d726b82a323bae02ae14ede9d9e4a
Jashandeep Singh <jashandeep1659@gmail.com>
2026-03-01 19:22:39 +0530

    feat: setup of opencode

--- core/internal/bootstrap/app.go
+++ core/internal/bootstrap/app.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/jashandeep31/vibeongo/core/internal/bootstrap/provision/docker"
+	"github.com/jashandeep31/vibeongo/core/internal/bootstrap/provision/opencode"
 	"github.com/jashandeep31/vibeongo/core/internal/bootstrap/provision/user"
 )
 
@@ -29,13 +29,24 @@ func Run() {
 	for _, pkg := range config.Packages {
 		switch pkg.Name {
 
-		case "docker":
-			if err := docker.Installer(pkg, config.SystemUser); err != nil {
-				log.Fatalf("docker install flow failed: %v", err)
+		// case "docker":
+		// 	fmt.Println("Setting up the docker")
+		// 	if err := docker.Installer(pkg, config.SystemUser); err != nil {
+		// 		log.Fatalf("docker install flow failed: %v", err)
+		// 	}
+		case "opencode":
+			fmt.Println("Setting up the opencode")
+			if err := opencode.SetupOpenCode(pkg); err != nil {
+				log.Fatalf("opencode installation failed: %v", err)
 			}
 
 		default:
 			fmt.Printf("%s is missing\n", pkg.Name)
 		}
 	}
+
+	// installing opencode
+	// sending the terminal to ui
+	// settings the cloudflare or use something diff to just make the ssl connections
+	// setting up the nvim with the kickstart
 }
