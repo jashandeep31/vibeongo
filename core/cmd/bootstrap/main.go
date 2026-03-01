72dca8acba920dc25fe4ab78324ca94a90566881
Jashandeep Singh <jashandeep1659@gmail.com>
2026-03-01 19:22:39 +0530

    feat: setup of opencode

--- core/cmd/bootstrap/main.go
+++ core/cmd/bootstrap/main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"github.com/jashandeep31/vibeongo/core/internal/bootstrap"
-)
+import "github.com/jashandeep31/vibeongo/core/internal/bootstrap"
 
 func main() {
 	// Init to the bootstrap script
