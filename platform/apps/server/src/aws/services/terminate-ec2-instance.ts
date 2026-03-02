48c0fed52a0de2d897d5f7c7fc6afc0906250c20
Jashandeep Singh <jashandeep1659@gmail.com>
2026-03-02 22:28:13 +0530

    feat: aws setup for diff regions

--- platform/apps/server/src/aws/services/terminate-ec2-instance.ts
+++ platform/apps/server/src/aws/services/terminate-ec2-instance.ts
@@ -1,13 +1,15 @@
 import { TerminateInstancesCommand } from "@aws-sdk/client-ec2";
 import { getEc2Client } from "../ec2-client.js";
+import { awsSupportedRegions } from "../configs/aws-supported-regions-configs.js";
 
 /**
  * terminate the ec2 instance
  */
-export const terminateEc2Instance = async (ids: string[]) => {
+export const terminateEc2Instance = async (region: string, ids: string[]) => {
   const command = new TerminateInstancesCommand({
     InstanceIds: ids,
   });
-  const client = getEc2Client("us-east-1");
+  // Temp solution of types may need to fix
+  const client = getEc2Client(region as (typeof awsSupportedRegions)[number]);
   return await client.send(command);
 };
