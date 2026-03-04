27cf5018b9033003f96e998071aa692e2b191297
Jashandeep Singh <jashandeep1659@gmail.com>
2026-03-04 09:14:23 +0530

    feat: setup of ws

--- platform/apps/web/app/(root)/page.tsx
+++ platform/apps/web/app/(root)/page.tsx
@@ -1,6 +1,19 @@
+"use client";
 import { Button } from "@repo/ui/components/button";
+import { useEffect } from "react";
 
-export default function page() {
+export default function Page() {
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8000/ws");
+    ws.onmessage = (event) => {
+      console.log(event);
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
   return (
     <div>
       <h1>This is waste of time page</h1>
