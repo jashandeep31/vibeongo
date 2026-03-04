97eb8b9a1cbf60753c67441e89daf598fa7c9919
Jashandeep Singh <jashandeep1659@gmail.com>
2026-03-04 19:12:37 +0530

    feat: dashboard page

--- platform/apps/web/app/dashboard/layout.tsx
+++ platform/apps/web/app/dashboard/layout.tsx
@@ -1,11 +1,16 @@
 import DashboardNavbar from "@/components/dashboard-navbar";
+import { DashboardSidebar } from "@/components/dashboard-sidebar";
+import { SidebarProvider } from "@repo/ui/components/sidebar";
 import React from "react";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <DashboardNavbar />
-      {children}
+      <SidebarProvider>
+        <DashboardSidebar />
+        <div className="w-full grid mt-12">{children}</div>
+      </SidebarProvider>
     </div>
   );
 }
--- platform/apps/web/components/dashboard-navbar.tsx
+++ platform/apps/web/components/dashboard-navbar.tsx
@@ -1,7 +1,7 @@
 export default function DashboardNavbar() {
   return (
-    <div className="border-b">
-      <div className="container py-3 ">
+    <div className="border-b h-14 flex items-center fixed top-0 w-full bg-background">
+      <div className="container">
         <h2 className="text-lg font-bold">VOG</h2>
       </div>
     </div>
