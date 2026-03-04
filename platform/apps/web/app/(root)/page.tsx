33221a6b39efe79dd428988d0d08960078bfed4f
Jashandeep Singh <jashandeep1659@gmail.com>
2026-03-04 19:12:37 +0530

    feat: dashboard page

--- platform/apps/web/app/(root)/page.tsx
+++ platform/apps/web/app/(root)/page.tsx
@@ -7,7 +7,7 @@ import "@xterm/xterm/css/xterm.css";
 import { FitAddon } from "@xterm/addon-fit";
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_SERVER_URL ?? "http://localhost:8080";
+  process.env.NEXT_PUBLIC_SERVER_URL ?? "http://192.168.1.69:8080";
 
 const WS_URL = (() => {
   try {
