f627a15afd322292340364b1ab24bc4821e8f458
Jashandeep Singh <jashandeep1659@gmail.com>
2026-03-05 19:44:01 +0530

    feat: basic server layout setup

--- platform/apps/server/src/test.ts
+++ platform/apps/server/src/test.ts
@@ -1,6 +1,3 @@
-import { DescribeInstancesCommand } from "@aws-sdk/client-ec2";
-import { getEc2Client } from "./aws/ec2-client.js";
-
 export default async function test() {
   console.log("Getting the ip from the server");
   if (1 == 1) return;
