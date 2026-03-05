91317b3d3dfff4a9f82a499e1682074fb36a9dd8
Jashandeep Singh <jashandeep1659@gmail.com>
2026-03-05 18:30:11 +0530

    feat: getting public ip of instance

--- platform/apps/server/src/test.ts
+++ platform/apps/server/src/test.ts
@@ -1,29 +1,9 @@
+import { DescribeInstancesCommand } from "@aws-sdk/client-ec2";
+import { getEc2Client } from "./aws/ec2-client.js";
+
 export default async function test() {
-  // await getAWSLinuxAmis();
-  // if (ec2s.filter((ec) => ec.status === "running").length >= 1) {
-  //   console.log(`already running ec2 look into billing i can't spin more `);
-  //   const ids: string[] = ec2s.map((instance) => instance.ec2_id);
-  //   const res = await terminateEc2Instance(ids);
-  //   console.log(res);
-  //   return;
-  // }
-  // const ec2res = await createEc2Instance({
-  //   region: "us-east-1",
-  // });
-  //
-  // console.log(ec2res);
-  // ec2res.Instances?.map(async (instance) => {
-  //   if (instance.InstanceId) {
-  //     await db.insert(ec2).values({
-  //       ec2_id: instance.InstanceId,
-  //       region: "us-east-1",
-  //       ip: instance.PrivateIpAddress || null,
-  //       status: "running",
-  //     });
-  //   }
-  // });
-  //
-  // await getAWSLinuxAmis();
-  //
+  console.log("Getting the ip from the server");
+  if (1 == 1) return;
+
   console.log(`Test server in running`);
 }
