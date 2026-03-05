919ecdb19c275cf47f57b908c6e6691c2223726f
Jashandeep Singh <jashandeep1659@gmail.com>
2026-03-05 19:44:01 +0530

    feat: basic server layout setup

---
+++ platform/apps/web/app/server/layout.tsx
@@ -0,0 +1,12 @@
+import { ServerSidebar } from "@/components/server-sidebar";
+import { SidebarProvider } from "@repo/ui/components/sidebar";
+import React from "react";
+
+export default function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <ServerSidebar />
+      <main>{children}</main>
+    </SidebarProvider>
+  );
+}
