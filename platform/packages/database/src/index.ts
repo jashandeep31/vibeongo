783a1b8d34fe03704956eb0a5808f5d7597e1c5c
Jashandeep Singh <jashandeep1659@gmail.com>
2026-03-05 11:10:14 +0530

    feat: project sessions schema

--- platform/packages/database/src/index.ts
+++ platform/packages/database/src/index.ts
@@ -12,3 +12,5 @@ export * from "./schemas/temp-ec2.js";
 export * from "./schemas/user.js";
 export * from "./schemas/environments.js";
 export * from "./schemas/projects.js";
+export * from "./schemas/project-sessions.js";
+export * from "./schemas/instances.js";
