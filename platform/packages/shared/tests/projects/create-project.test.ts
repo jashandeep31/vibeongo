233d178206100c7e159c498bc2c88449d354d8da
Jashandeep Singh <jashandeep1659@gmail.com>
2026-03-07 16:01:52 +0530

    feat: working tests

--- platform/packages/shared/tests/projects/create-project.test.ts
+++ platform/packages/shared/tests/projects/create-project.test.ts
@@ -1,7 +1,20 @@
 import { expect, test } from "vitest";
-import { projectConfigValidator, z } from "@repo/shared";
+import { projectConfigValidator } from "@repo/shared";
+import fs from "fs";
+
+const currentDir = import.meta.dirname;
 test("Full config pass", () => {
-  const config = {};
-  const parsedResponse = projectConfigValidator.safeParse(config);
-  expect(parsedResponse.error).toBe(null);
+  const files = fs.readdirSync(`${currentDir}/configs`);
+  for (const fileName of files) {
+    const fileContent = fs.readFileSync(`${currentDir}/configs/${fileName}`, {
+      encoding: "utf8",
+    });
+    const parsedResponse = projectConfigValidator.safeParse(
+      JSON.parse(fileContent),
+    );
+    if (!parsedResponse.success) {
+      console.log(JSON.stringify(parsedResponse, null, 2));
+    }
+    expect(parsedResponse.success).toBe(true);
+  }
 });
