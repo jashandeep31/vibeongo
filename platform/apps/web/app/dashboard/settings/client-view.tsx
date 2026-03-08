b45cd95c8a79363446387005bc5cf9641b2cebb4
Jashandeep Singh <jashandeep1659@gmail.com>
2026-03-08 16:14:18 +0530

    feat: dialog of create shh key

--- platform/apps/web/app/dashboard/settings/client-view.tsx
+++ platform/apps/web/app/dashboard/settings/client-view.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useSshKeys } from "@/hooks/use-shh-keys";
+import { CreateSshKeyDialog } from "@/components/dialogs/create-ssh-key-dialog";
 
 export default function ClientView() {
   const { data: sshKeys } = useSshKeys();
@@ -8,9 +9,12 @@ export default function ClientView() {
     <div className="p-8">
       <h1 className="text-3xl font-bold tracking-tight">Settings</h1>
       <div className="mt-8">
-        <h2 className="text-lg  font-semibold text-muted-foreground">
-          SSH keys
-        </h2>
+        <div className="flex justify-between">
+          <h2 className="text-lg  font-semibold text-muted-foreground">
+            SSH keys
+          </h2>
+          <CreateSshKeyDialog />
+        </div>
       </div>
     </div>
   );
