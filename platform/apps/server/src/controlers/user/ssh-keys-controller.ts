c2a952d52fb7cc1e4390b9d8bf5907ee43086f60
Jashandeep Singh <jashandeep1659@gmail.com>
2026-03-08 16:57:39 +0530

    feat: delete ssh keys

--- platform/apps/server/src/controlers/user/ssh-keys-controller.ts
+++ platform/apps/server/src/controlers/user/ssh-keys-controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { catchAsync } from "../../lib/catch-async.js";
 import { AppError } from "../../lib/appError.js";
 import { z } from "zod";
-import { db, eq, sshKeys } from "@repo/db";
+import { db, eq, and, sshKeys } from "@repo/db";
 import { createSshKeySchema } from "@repo/shared";
 
 export const createSshKey = catchAsync(async (req: Request, res: Response) => {
@@ -13,6 +13,7 @@ export const createSshKey = catchAsync(async (req: Request, res: Response) => {
   await db.insert(sshKeys).values({
     name: parsedData.name,
     value: parsedData.value,
+    user_id: user.id,
   });
 
   res.status(201).json({
@@ -27,9 +28,31 @@ export const getSshKeys = catchAsync(async (req: Request, res: Response) => {
   const sshKeysRes = await db
     .select()
     .from(sshKeys)
-    .where(eq(sshKeys.id, user.id));
+    .where(eq(sshKeys.user_id, user.id));
 
   res.status(200).json({
     data: sshKeysRes,
+  });
+});
+
+export const deleteSshKey = catchAsync(async (req: Request, res: Response) => {
+  const user = req.user;
+  if (!user) throw new AppError("Authentication is required", 401);
+
+  const id = req.params.id;
+  if (!id || typeof id !== "string")
+    throw new AppError("SSH Key ID is required", 400);
+
+  const result = await db
+    .delete(sshKeys)
+    .where(and(eq(sshKeys.id, id), eq(sshKeys.user_id, user.id)))
+    .returning();
+
+  if (result.length === 0) {
+    throw new AppError("SSH key not found or unauthorized", 404);
+  }
+
+  res.status(200).json({
+    message: "SSH key deleted successfully",
   });
 });
