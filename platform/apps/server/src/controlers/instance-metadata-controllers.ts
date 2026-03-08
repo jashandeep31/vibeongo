f6ad3c36ed017ac9803f0799a782248a25b575d3
Jashandeep Singh <jashandeep1659@gmail.com>
2026-03-08 11:59:26 +0530

    feat: rendering of vps

--- platform/apps/server/src/controlers/instance-metadata-controllers.ts
+++ platform/apps/server/src/controlers/instance-metadata-controllers.ts
@@ -1,12 +1,31 @@
 import { Request, Response } from "express";
 import { catchAsync } from "../lib/catch-async.js";
-import { db, instanceRegions } from "@repo/db";
+import { db, instanceRegions, eq, instanceTypes } from "@repo/db";
+import { AppError } from "../lib/appError.js";
 
 export const getIntanceRegions = catchAsync(
   async (_req: Request, res: Response) => {
     const regions = await db.select().from(instanceRegions);
     res.status(200).json({
       data: regions,
+    });
+    return;
+  },
+);
+
+export const getInstanceTypesByRegion = catchAsync(
+  async (req: Request, res: Response) => {
+    const { regionId } = req.params;
+    if (!regionId || typeof regionId !== "string")
+      throw new AppError("Region ID is required", 400);
+
+    const instanceTypesData = await db
+      .select()
+      .from(instanceTypes)
+      .where(eq(instanceTypes.region_id, regionId));
+
+    res.status(200).json({
+      data: instanceTypesData,
     });
     return;
   },
--- platform/apps/server/src/routes/instance-metadata-routes.ts
+++ platform/apps/server/src/routes/instance-metadata-routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
-import { getIntanceRegions } from "../controlers/instance-metadata-controllers.js";
+import {
+  getIntanceRegions,
+  getInstanceTypesByRegion,
+} from "../controlers/instance-metadata-controllers.js";
 
 const routes: Router = Router();
 
 routes.route("/regions").get(getIntanceRegions);
+routes.route("/instance-types/:regionId").get(getInstanceTypesByRegion);
+
 export const instanceMetadataRoutes = routes;
