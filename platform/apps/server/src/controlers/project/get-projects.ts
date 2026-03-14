c5781bfb9df15b9633a3d68803cf380a1b0bcc20
Jashandeep Singh <jashandeep1659@gmail.com>
2026-03-14 22:09:43 +0530

    feat: get project by id functionality

--- platform/apps/server/src/controlers/project/get-projects.ts
+++ platform/apps/server/src/controlers/project/get-projects.ts
@@ -1,4 +1,4 @@
-import { db, eq, projects } from "@repo/db";
+import { and, db, eq, projects } from "@repo/db";
 import { AppError } from "../../lib/appError.js";
 import { catchAsync } from "../../lib/catch-async.js";
 import { Request, Response } from "express";
@@ -18,3 +18,24 @@ export const getProjects = catchAsync(async (req: Request, res: Response) => {
     data: dbProjects,
   });
 });
+
+export const getProjectById = catchAsync(
+  async (req: Request, res: Response) => {
+    const user = req.user;
+    if (!user) throw new AppError("authentication is required", 401);
+
+    const { id } = req.params;
+    if (!id || typeof id !== "string")
+      throw new AppError("project id is required", 400);
+
+    const [dbProject] = await db
+      .select()
+      .from(projects)
+      .where(and(eq(projects.user_id, user.id), eq(projects.id, id)));
+
+    res.status(200).json({
+      message: "Project retrieved successfully",
+      data: dbProject,
+    });
+  },
+);
--- platform/apps/server/src/routes/project-routes.ts
+++ platform/apps/server/src/routes/project-routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { createProject } from "../controlers/project/create-project.js";
 import { checkAuthorization } from "../lib/check-authorization.js";
-import { getProjects } from "../controlers/project/get-projects.js";
+import {
+  getProjectById,
+  getProjects,
+} from "../controlers/project/get-projects.js";
 
 const routes: Router = Router();
 
-routes.route("/:id");
+routes.route("/:id").get(checkAuthorization(["all"]), getProjectById);
+
 routes
   .route("/")
   .post(checkAuthorization(["all"]), createProject)
