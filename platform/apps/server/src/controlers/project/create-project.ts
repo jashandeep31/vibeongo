3618cb4453c59c0d9b03fd3ac77b9fbf26e3322e
Jashandeep Singh <jashandeep1659@gmail.com>
2026-03-14 22:00:50 +0530

    feat: getting projects route

--- platform/apps/server/src/controlers/project/create-project.ts
+++ platform/apps/server/src/controlers/project/create-project.ts
@@ -2,24 +2,33 @@ import { Request, Response } from "express";
 import { catchAsync } from "../../lib/catch-async.js";
 import { AppError } from "../../lib/appError.js";
 import { db, projects } from "@repo/db";
-import { projectConfigValidator, z } from "@repo/shared";
+import { projectConfigValidator } from "@repo/shared";
 
 export const createProject = catchAsync(async (req: Request, res: Response) => {
   const user = req.user;
   if (!user) {
     throw new AppError("authentication is required", 401);
   }
-  console.log(req.body);
+  const { body } = req;
 
+  const parsedData = projectConfigValidator.parse(body);
   const project = await db.transaction(async (tx) => {
-    // create the project with the stoped state
-    // just save the config
-    const { body } = req;
-    const parsedData = projectConfigValidator.parse(body);
-    // const config = parsedData.config;
+    return await tx
+      .insert(projects)
+      .values({
+        name: parsedData.name,
+        description: parsedData.description,
+        status: "running",
+        user_id: user.id,
+        instance_type_id: parsedData.instanceTypeId,
+        total_charges: 0,
+        config: parsedData.config,
+      })
+      .returning();
   });
 
   res.status(200).json({
     message: "Project created successfully",
+    data: project,
   });
 });
