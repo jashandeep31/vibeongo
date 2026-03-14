350ffc294b132c4e08b4cd3aa3f5f44323a3d5b5
Jashandeep Singh <jashandeep1659@gmail.com>
2026-03-14 12:12:35 +0530

    feat: project create setup

--- platform/apps/server/src/routes/project-routes.ts
+++ platform/apps/server/src/routes/project-routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { createProject } from "../controlers/project/create-project.js";
+import { checkAuthorization } from "../lib/check-authorization.js";
 
 const routes: Router = Router();
 
 routes.route("/:id");
-routes.route("/").post(createProject);
+routes.route("/").post(checkAuthorization(["all"]), createProject);
 
 export const projectRoutes = routes;
