b27146ee93eca41851e1a5f617034153709fb0f3
Jashandeep Singh <jashandeep1659@gmail.com>
2026-03-15 23:15:46 +0530

    feat: new config.json

--- core/internal/bootstrap/validate_config.go
+++ core/internal/bootstrap/validate_config.go
@@ -2,6 +2,7 @@ package bootstrap
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 
 	"github.com/jashandeep31/vibeongo/core/internal/config"
@@ -13,5 +14,17 @@ func ValidateConfig(file []byte) (config.Config, error) {
 		return cfg, fmt.Errorf("Error in parsing the config: %w", err)
 	}
 
-	return cfg, nil
+	// validating the docker config
+
+	for _, pkg := range cfg.Packages {
+		if pkg.Name == "docker" {
+			fmt.Println("docker is here")
+			fmt.Println(string(pkg.Config))
+		}
+	}
+	// TODO:  move all the validation to the config file here so no shit can happen on the go
+	// TODO: use colors to print the validation and other messages
+
+	return cfg, errors.New("this is custom generated error")
+	// return cfg, nil
 }
