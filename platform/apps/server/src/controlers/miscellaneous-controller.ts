049ef61a82715c387d01a7f015beaf3219321a25
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-03-19 11:30:27 +0530

    feat: streaming file instead of loading all to memory

--- platform/apps/server/src/controlers/miscellaneous-controller.ts
+++ platform/apps/server/src/controlers/miscellaneous-controller.ts
@@ -4,18 +4,20 @@ import path from "node:path";
 import fs from "node:fs";
 
 const RootPath = process.cwd();
+
 export const serveBootstrapServer = catchAsync(
-  async (req: Request, res: Response) => {
-    console.log("the request is here");
-    const BinaryPath = path.join(RootPath, "../../../core/tmp/bootstrap");
-    res.download(BinaryPath);
+  async (_req: Request, res: Response) => {
+    const binaryPath = path.join(RootPath, "../../../core/tmp/bootstrap");
+
+    const stat = fs.statSync(binaryPath);
+
+    res.writeHead(200, {
+      "Content-Type": "",
+      "Content-Length": stat.size,
+    });
+
+    const stream = fs.createReadStream(binaryPath);
+
+    stream.pipe(res);
   },
 );
-
-// const RootPath = process.cwd();
-// export const serveBootstrapServer = async () => {
-//   const BinaryPath = path.join(RootPath, "../../../core/cmd/bootstrap");
-//
-//   console.log("serving hte file ");
-//
-// };
