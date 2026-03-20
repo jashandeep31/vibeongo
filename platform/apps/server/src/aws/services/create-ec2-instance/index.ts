0ee9843095e46016e5ab56b882f5f81d38ba8a59
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-03-20 20:31:18 +0530

    feat: binary serving routes and new bash script

--- platform/apps/server/src/aws/services/create-ec2-instance/index.ts
+++ platform/apps/server/src/aws/services/create-ec2-instance/index.ts
@@ -35,6 +35,8 @@ exec > /var/log/user-data.log 2>&1
 
 USER_HOME="/home/ubuntu"
 
+echo "Step 1: Setup SSH"
+
 mkdir -p "$USER_HOME/.ssh"
 
 echo "${env.SSH_KEY}" >> "$USER_HOME/.ssh/authorized_keys"
@@ -44,14 +46,33 @@ chmod 600 "$USER_HOME/.ssh/authorized_keys"
 
 chown -R ubuntu:ubuntu "$USER_HOME/.ssh"
 
-echo "SSH key added successfully
+echo "Step 2: Download install script"
 
-curl -fL https://l1.devsradar.com/install -o /home/ubuntu/install
+curl -fL https://l1.devsradar.com/install -o "$USER_HOME/install"
 
-chmod +x /home/ubuntu/install
+curl -fL https://l1.devsradar.com/install -o "$USER_HOME/install-api"
 
-/home/ubuntu/install
-"`,
+echo "Step 3: Download config"
+
+curl -fL https://l1.devsradar.com/config -o "$USER_HOME/config.json"
+
+chmod +x "$USER_HOME/install"
+
+echo "Step 4: Run install script"
+
+# Run as ubuntu user (IMPORTANT)
+sudo -u ubuntu bash -c "cd /home/ubuntu && /home/ubuntu/install"
+
+echo "Step 5: Done"
+
+touch "$USER_HOME/done.txt"
+echo "We are done" > "$USER_HOME/done.txt"
+echo "Plese reboot the server onces" > "$USER_HOME/done.txt"
+
+source ~/.bashrc
+newgrp docker
+
+`,
     ).toString("base64"),
   });
   const client = getEc2Client(region);
