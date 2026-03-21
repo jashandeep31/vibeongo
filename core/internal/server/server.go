201601dda1728989824c91d48ad901a74083a30f
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-03-21 12:09:25 +0530

    feat: working db and clean up

--- core/internal/server/server.go
+++ core/internal/server/server.go
@@ -4,10 +4,14 @@ import (
 	"database/sql"
 	"log"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/sqlite"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jashandeep31/vibeongo/core/internal/config"
 	"github.com/jashandeep31/vibeongo/core/internal/routes"
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
+	_ "modernc.org/sqlite"
 )
 
 func Start() {
@@ -17,12 +21,22 @@ func Start() {
 	e := echo.New()
 	e.Use(middleware.RequestLogger())
 
-	_, err := sql.Open("sqlite", "./test.sql")
+	dbConn, err := sql.Open("sqlite", "./test.db")
 	if err != nil {
 		log.Fatalf("Failed to connect the db")
 	}
+	defer dbConn.Close()
+
+	m, err := migrate.New("file://internal/db/migrations", "sqlite://test.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal(err)
+	}
+
 	// routes of app
-	routes.RegisterMiscRoutes(e)
+	routes.RegisterMiscRoutes(e, dbConn)
 	routes.RegisterWSRoutes(e)
 
 	address := ":" + config.ENV.PORT
