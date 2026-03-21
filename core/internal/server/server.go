494c0c90be285a9485c1e97d6e30bf2c4ba55bdf
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-03-22 00:59:45 +0530

    feat: migration files fix in binary

--- core/internal/server/server.go
+++ core/internal/server/server.go
@@ -2,36 +2,29 @@ package server
 
 import (
 	"database/sql"
+	"embed"
 	"log"
-	"os"
-	"path/filepath"
 
 	"github.com/golang-migrate/migrate/v4"
-	_ "github.com/golang-migrate/migrate/v4/database/sqlite"
+
+	// _ "github.com/golang-migrate/migrate/v4/database/sqlite"
+	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
-	"github.com/jashandeep31/vibeongo/core/internal/config"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 	"github.com/jashandeep31/vibeongo/core/internal/routes"
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
 	_ "modernc.org/sqlite"
 )
 
+var migrationsFS embed.FS
+
 func Start() {
 	// loading the env
-	config.LoadEnv()
+	// config.LoadEnv()
 
 	e := echo.New()
 	e.Use(middleware.RequestLogger())
-
-	wd, _ := os.Getwd()
-	migrationsPath := "file://" + filepath.Join(wd, "internal/db/migrations")
-	m, err := migrate.New(migrationsPath, "sqlite://test.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-		log.Fatal(err)
-	}
 
 	dbConn, err := sql.Open("sqlite", "./test.db")
 	if err != nil {
@@ -42,8 +35,29 @@ func Start() {
 	routes.RegisterMiscRoutes(e, dbConn)
 	routes.RegisterWSRoutes(e)
 
-	address := ":" + config.ENV.PORT
+	// address := ":" + config.ENV.PORT
+	address := ":" + "8000"
 	if err := e.Start(address); err != nil {
 		e.Logger.Error("failed to start server", "error", err)
+	}
+}
+
+func runMigrations() {
+	d, err := iofs.New(migrationsFS, "internal/db/migrations")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.NewWithSourceInstance(
+		"iofs",
+		d,
+		"sqlite3:///home/ubuntu/test.db", // IMPORTANT: absolute path
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal(err)
 	}
 }
