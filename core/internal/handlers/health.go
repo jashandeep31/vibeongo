02a697479bea34c7d87f413642226dd59dade994
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-03-21 12:09:25 +0530

    feat: working db and clean up

--- core/internal/handlers/health.go
+++ core/internal/handlers/health.go
@@ -6,13 +6,11 @@ import (
 	"github.com/labstack/echo/v5"
 )
 
-type HealthResponse struct {
-	Message string `json:"message"`
-}
-
 func Health(c *echo.Context) error {
 	version := "v0.0.1"
-	return c.JSON(http.StatusOK, HealthResponse{
+	return c.JSON(http.StatusOK, struct {
+		Message string `json:"message"`
+	}{
 		Message: "Hi, Your sandbox server is running " + version,
 	})
 }
--- core/internal/routes/misc.go
+++ core/internal/routes/misc.go
@@ -1,10 +1,12 @@
 package routes
 
 import (
+	"database/sql"
+
 	"github.com/jashandeep31/vibeongo/core/internal/handlers"
 	"github.com/labstack/echo/v5"
 )
 
-func RegisterMiscRoutes(e *echo.Echo) {
+func RegisterMiscRoutes(e *echo.Echo, db *sql.DB) {
 	e.GET("/", handlers.Health)
 }
