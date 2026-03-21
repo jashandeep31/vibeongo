4a813f93829f5bbf8e551d92853ab1af2a0f3bf6
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-03-21 11:02:16 +0530

    feat: db setup

---
+++ core/internal/db/migrations/users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
