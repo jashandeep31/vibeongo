8b598e63a42997db6ce7f67a892e9c2cbe0c49de
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-03-23 23:18:24 +0530

    feat: instance detail page

--- platform/apps/server/src/controlers/instance/get-instances.ts
+++ platform/apps/server/src/controlers/instance/get-instances.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import { catchAsync } from "../../lib/catch-async.js";
 import { AppError } from "../../lib/appError.js";
-import { db, eq, instances } from "@repo/db";
+import { and, db, eq, instances } from "@repo/db";
+import { catchall } from "zod/mini";
 
-export const getUserIntances = catchAsync(
+export const getUserInstances = catchAsync(
   async (req: Request, res: Response) => {
     const user = req.user;
     if (!user) throw new AppError("authentication is required", 400);
@@ -15,6 +16,26 @@ export const getUserIntances = catchAsync(
 
     res.status(200).json({
       data: rows,
+    });
+  },
+);
+
+export const getInstanceById = catchAsync(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    if (typeof id !== "string") throw new AppError("Id should be string ", 400);
+    const user = req.user;
+    if (!user) throw new AppError("authentication is required", 400);
+
+    const [row] = await db
+      .select()
+      .from(instances)
+      .where(and(eq(instances.user_id, user.id), eq(instances.id, id)));
+
+    if (!row) throw new AppError("instance not found", 404);
+
+    res.status(200).json({
+      data: row,
     });
   },
 );
