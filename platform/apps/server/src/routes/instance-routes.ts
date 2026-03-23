af8a9bc67fbd8bbd47201e9b8892275961290129
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-03-23 20:36:46 +0530

    feat: creation of instance from frontend wokring

--- platform/apps/server/src/routes/instance-routes.ts
+++ platform/apps/server/src/routes/instance-routes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { checkAuthorization } from "../lib/check-authorization.js";
+import { createInstance } from "../controlers/instance/create-instance.js";
 
 const routes: Router = Router();
+routes.route("/").post(checkAuthorization(["all"]), createInstance);
 
 export const instanceRoutes = routes;
