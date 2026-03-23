63dae0999d80caeb85a949e1fc6d9c848dac539f
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-03-23 20:36:46 +0530

    feat: creation of instance from frontend wokring

---
+++ platform/apps/web/services/instance-servies.ts
@@ -0,0 +1,11 @@
+import { BACKEND_URL } from "@/lib/constants";
+import axios from "axios";
+
+export const createInstance = async (
+  data: unknown,
+): Promise<{ message: string }> => {
+  const res = await axios.post(`${BACKEND_URL}/api/v1/instance`, data, {
+    withCredentials: true,
+  });
+  return res.data;
+};
