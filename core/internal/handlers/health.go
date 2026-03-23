3320115ddd7195a1c5bf0f163b860d13a3f79d65
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-03-23 16:40:49 +0530

    feat: version system

--- core/internal/handlers/health.go
+++ core/internal/handlers/health.go
@@ -6,11 +6,17 @@ import (
 	"github.com/labstack/echo/v5"
 )
 
+var (
+	Version   = "dev"
+	BuildTime = "unknown"
+)
+
 func Health(c *echo.Context) error {
-	version := "v0.0.6"
 	return c.JSON(http.StatusOK, struct {
 		Message string `json:"message"`
+		V       string `json:"v"`
 	}{
-		Message: "Hi, Your sandbox server is running " + version,
+		Message: "Hi, Your sandbox server is running",
+		V:       Version + " " + BuildTime,
 	})
 }
