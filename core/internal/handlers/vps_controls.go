55ba28b319f2e79ba829b2bb468d5e5d8f3e5722
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-03-23 16:26:36 +0530

    chore: clean up

--- core/internal/handlers/vps_controls.go
+++ core/internal/handlers/vps_controls.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 	"os/exec"
 	"time"
@@ -20,43 +19,5 @@ func RebootServer(c *echo.Context) error {
 		Message string `json:"messge"`
 	}{
 		Message: "Server is getting restarted in 2sec",
-	})
-}
-
-func UpdateApiServer(c *echo.Context) error {
-	go func() {
-		fmt.Println("things are running in the background")
-		cmd := exec.Command("sudo", "bash", "-c", `
-set -e
-
-USER_HOME="/home/ubuntu"
-BIN="$USER_HOME/server"
-NEW="$USER_HOME/server.new"
-
-echo "[1/5] Downloading new binary..."
-curl -fL https://l1.devsradar.com/install-api -o "$NEW"
-
-echo "[2/5] Making it executable..."
-chmod +x "$NEW"
-
-echo "[3/5] Stopping service..."
-systemctl stop myserver
-
-echo "[4/5] Replacing binary..."
-mv -f "$NEW" "$BIN"
-
-echo "[5/5] Starting service..."
-systemctl start myserver
-
-echo "Update completed successfully"
-`)
-
-		err := cmd.Run()
-		fmt.Println(err)
-	}()
-	return c.JSON(http.StatusOK, struct {
-		Message string `json:"message"`
-	}{
-		Message: "Updating the server please send requests late",
 	})
 }
