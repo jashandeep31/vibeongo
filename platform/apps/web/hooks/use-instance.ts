e811062b545e94aaad27012c14b1e1609acc6fa5
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-03-25 23:06:44 +0530

    feat: termination of instance

--- platform/apps/web/hooks/use-instance.ts
+++ platform/apps/web/hooks/use-instance.ts
@@ -2,8 +2,9 @@ import {
   createInstance,
   getInstanceById,
   getInstances,
-} from "@/services/instance-servies";
-import { useMutation, useQuery } from "@tanstack/react-query";
+  terminateInstance,
+} from "@/services/instance-services";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const useCreateInstance = () =>
   useMutation({
@@ -22,3 +23,16 @@ export const useGetInstanceById = (id: string) =>
     queryFn: () => getInstanceById(id),
     enabled: !!id,
   });
+
+export const useTerminateInstance = (id: string) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (instanceId: string) => terminateInstance(instanceId),
+    onSuccess: (_, instanceId) => {
+      queryClient.invalidateQueries({ queryKey: ["project", id] });
+      queryClient.invalidateQueries({ queryKey: ["instances"] });
+      queryClient.invalidateQueries({ queryKey: ["instance", instanceId] });
+    },
+  });
+};
