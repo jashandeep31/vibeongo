7f61d780d509b8e0c04730d9d0ec0e7063459454
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-03-28 13:24:23 +0530

    fix: vibeongo folder under ubuntu user instead of root user

--- core/internal/bootstrap/app.go
+++ core/internal/bootstrap/app.go
@@ -3,8 +3,6 @@ package bootstrap
 import (
 	"fmt"
 	"log"
-	"os/exec"
-	"sync"
 
 	"github.com/fatih/color"
 	"github.com/jashandeep31/vibeongo/core/internal/bootstrap/provision/docker"
@@ -16,7 +14,6 @@ import (
 )
 
 func Run() {
-	var wg sync.WaitGroup
 	fmt.Println("v0.0.5")
 	fmt.Println("")
 	color.Cyan("Welcome, We are setting the system up for you")
@@ -43,26 +40,6 @@ func Run() {
 		nvim.Setup(cfg.Nvim)
 	}
 
-	if cfg.Task != "" {
-		wg.Add(1)
-		go func() {
-			sourceCmd := exec.Command("source", "/home/ubuntu/.bashrc")
-			sourceCmd.Run()
-			defer wg.Done()
-
-			fmt.Println("Working on the opencode task")
-			opencodeCommand := fmt.Sprintf("/home/ubuntu/.opencode/bin/opencode run %s", cfg.Task)
-			cmd := exec.Command("bash", "-c", opencodeCommand)
-			cmd.Dir = "/home/ubuntu/code"
-			out, err := cmd.CombinedOutput()
-			if err != nil {
-				fmt.Println(err)
-			}
-			fmt.Println("Done with the opencode task", string(out))
-			fmt.Println("Done with the opencode task")
-		}()
-	}
-	wg.Wait()
 	fmt.Println("writing the bash scripts")
 	scripts.WriteScripts()
 }
