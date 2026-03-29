4a06a9d907c6466199a8078efb1ad0a4b9f990de
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-03-29 17:46:16 +0530

    feat: single script based setup

--- core/internal/bootstrap/provision/runtimes/setup.go
+++ core/internal/bootstrap/provision/runtimes/setup.go
@@ -1,43 +1,21 @@
 package runtimes
 
-import "os/exec"
+func NodeJSSetup() string {
+	script := `
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 
-func NodeJSSetup() {
-	script := `#!/usr/bin/env bash
-set -e
+# in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
 
-export NVM_DIR="$HOME/.nvm"
-
-# Install nvm if not present
-if [ ! -d "$NVM_DIR" ]; then
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
-fi
-
-# Load nvm
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-
-# Install + set default node
+# Download and install Node.js:
 nvm install 24
-nvm alias default 24
-nvm use default
 
-source $HOME/.nvm/nvm.sh
-source $HOME/.bashrc
-# Ensure it's active NOW
-node -v
-npm -v
+# Verify the Node.js version:
+node -v # Should print "v24.14.1".
 
-# Persist for ALL future shells
-PROFILE="$HOME/.bashrc"
-
-grep -q 'NVM_DIR' $PROFILE || echo 'export NVM_DIR="$HOME/.nvm"' >> $PROFILE
-grep -q 'nvm.sh' $PROFILE || echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"' >> $PROFILE
+# Verify npm version:
+npm -v # Should print "11.11.0".
 `
-
-	cmd := exec.Command("bash", "-c", script)
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		println("Failed to run the setup script", err)
-	}
-	println(string(out))
+	return script
 }
