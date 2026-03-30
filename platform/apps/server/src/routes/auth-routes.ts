6c50b7a07d3871669ae4a19a9f58c54d8fe3901e
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-03-30 16:08:19 +0530

    feat: setup of github auth

--- platform/apps/server/src/routes/auth-routes.ts
+++ platform/apps/server/src/routes/auth-routes.ts
@@ -3,10 +3,16 @@ import {
   googleAuthCallbackController,
   googleAuthUrl,
 } from "../controlers/auth/google-auth.js";
+import {
+  githubAuthCallbackController,
+  githubAuthUrl,
+} from "../controlers/auth/github-auth.js";
 
 const routes: Router = Router();
 routes.route("/google").get(googleAuthUrl);
 routes.route("/google/callback").get(googleAuthCallbackController);
+routes.route("/github").get(githubAuthUrl);
+routes.route("/github/callback").get(githubAuthCallbackController);
 // routes.route("/logout").get();
 // TODO: make the logout route
 export const authRoutes = routes;
