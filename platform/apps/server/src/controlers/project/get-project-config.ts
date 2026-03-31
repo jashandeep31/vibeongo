9c8f78bf4a53a4715242b240c66806e02fe1a8df
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-03-31 20:39:10 +0530

    feat: middleware for bearer token auth

--- platform/apps/server/src/controlers/project/get-project-config.ts
+++ platform/apps/server/src/controlers/project/get-project-config.ts
@@ -1,21 +1,20 @@
 import { Request, Response } from "express";
 import { catchAsync } from "../../lib/catch-async.js";
 import { AppError } from "../../lib/appError.js";
-import { db, eq, projects } from "@repo/db";
+import { and, db, eq, projects } from "@repo/db";
 
 export const getProjectConfigById = catchAsync(
   async (req: Request, res: Response) => {
-    const X_API_KEY = req.headers["x-api-id"];
-    const X_API_SECRET = req.headers["x-api-secret"];
-    if (!X_API_KEY || !X_API_SECRET)
-      throw new AppError("Authnatication is required", 400);
+    const user = req.user;
+    if (!user) throw new AppError("Authentication is required", 400);
+
     const id = req.params.id;
     if (!id || typeof id !== "string") throw new AppError("Invalid id ", 400);
 
     const [project] = await db
       .select()
       .from(projects)
-      .where(eq(projects.id, id));
+      .where(and(eq(projects.id, id), eq(projects.user_id, user.id)));
     if (!project) throw new AppError("Project not found", 404);
 
     res.status(200).json({ data: project.config });
