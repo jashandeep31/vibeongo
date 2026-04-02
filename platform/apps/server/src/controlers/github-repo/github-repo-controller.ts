7c08a42c2656980870f612d409a6ba136a302b3b
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-04-02 17:09:08 +0530

    feat: creating github repo controller

--- platform/apps/server/src/controlers/github-repo/github-repo-controller.ts
+++ platform/apps/server/src/controlers/github-repo/github-repo-controller.ts
@@ -2,7 +2,8 @@ import { AppError } from "../../lib/appError.js";
 import { catchAsync } from "../../lib/catch-async.js";
 import { Request, Response } from "express";
 import { octokitApp } from "../../webhooks/github/handler.js";
-import { checkAppInstallationInRepo } from "../../github-app-functions/check-appinstallation.js";
+import { getRepoAccessDetails } from "../../github-app-functions/get-repo-access-details.js";
+import { db, githubRepos } from "@repo/db";
 
 export const createGithubRepo = catchAsync(
   async (req: Request, res: Response) => {
@@ -12,17 +13,27 @@ export const createGithubRepo = catchAsync(
     const { url } = req.body;
     if (!url) throw new AppError("github repo url is required", 400);
 
-    const repoInfo = await checkAppInstallationInRepo({
+    const { hasAppAccess, isPrivate, repoData } = await getRepoAccessDetails({
       owner: "jashandeep31",
       repo: "aichat",
     });
-    //Things we have to do
-    // 1. Check we have repo access or not
-    // 3. IS the requrest is the real owner of the app
-    // 2. Check repo is public not or not
 
+    if (!hasAppAccess) throw new AppError("App access is required", 400);
+    if (user.username !== repoData.owner.login)
+      throw new AppError("You are not the owner of this repo", 400);
+
+    const repo = await db
+      .insert(githubRepos)
+      .values({
+        user_id: user.id,
+        full_name: repoData.full_name,
+        repo_owner_username: repoData.owner.login,
+        public: isPrivate,
+      })
+      .returning();
     res.status(201).json({
       message: "Successfully had created the project intance",
+      data: repo,
     });
   },
 );
