d0ee798709d406eaf8343f10107234ebd3fc10dd
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-04-04 11:55:40 +0530

    feat: nav menu

--- platform/apps/web/app/provider.tsx
+++ platform/apps/web/app/provider.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import NavCommandBox from "@/components/nav-command-box";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 
@@ -7,6 +8,11 @@ export const queryClient = new QueryClient();
 
 export default function Provider({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <>
+        <NavCommandBox />
+        {children}
+      </>
+    </QueryClientProvider>
   );
 }
---
+++ platform/apps/web/components/nav-command-box.tsx
@@ -0,0 +1,68 @@
+"use client";
+import React from "react";
+import {
+  Command,
+  CommandInput,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandItem,
+  CommandList,
+} from "@repo/ui/components/command";
+import { useRouter } from "next/navigation";
+const NavCommandBox = () => {
+  const router = useRouter();
+  const [open, setOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  const commandsList: { label: string; href: string; external?: boolean }[] = [
+    { label: "Dashboard", href: "/dashboard" },
+    { label: "My Projects", href: "/projects" },
+    { label: "Create Project", href: "/dashboard/project/create" },
+    { label: "Repos", href: "/dashboard/repos" },
+    { label: "SSH keys", href: "/dashboard/settings/ssh-keys" },
+    { label: "Auth Keys", href: "/dashboard/settings/auth-keys" },
+    { label: "Settings", href: "/dashboard/settings" },
+  ];
+
+  return (
+    <CommandDialog open={open} onOpenChange={setOpen} className="min-w-[500px]">
+      <Command>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Quick Actions">
+            {commandsList.map((command) => (
+              <CommandItem
+                className="p-1 text-sm"
+                key={command.label}
+                onSelect={() => {
+                  if (command.external) {
+                    window.open(command.href, "_blank", "noopener,noreferrer");
+                  } else {
+                    router.push(command.href);
+                  }
+                  setOpen(false);
+                }}
+              >
+                {command.label}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </CommandList>
+      </Command>
+    </CommandDialog>
+  );
+};
+
+export default NavCommandBox;
