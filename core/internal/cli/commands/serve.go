a3787976effd64f87ac7d7b6e2cd4b701a1f5d3c
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-04-07 23:07:37 +0530

    chore: file refactor

---
+++ core/internal/cli/commands/serve.go
@@ -0,0 +1,25 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/jashandeep31/vibeongo/core/internal/server"
+	"github.com/spf13/cobra"
+)
+
+func ServeCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "serve",
+		Short: "Start Server",
+		Long:  "Start the server at the port 8080 ",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return startServer()
+		},
+	}
+}
+
+func startServer() error {
+	err := server.Start()
+	fmt.Println(err)
+	return err
+}
--- core/internal/cli/root.go
+++ core/internal/cli/root.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/jashandeep31/vibeongo/core/internal/server"
+	"github.com/jashandeep31/vibeongo/core/internal/cli/commands"
 	"github.com/spf13/cobra"
 )
 
@@ -19,26 +19,9 @@ var rootCmd = &cobra.Command{
 func init() {
 	// rootCmd.AddCommand(commands.SetupCmd())
 	// rootCmd.AddCommand(commands.UpdateCmd())
-	// rootCmd.AddCommand(commands.TaskCmd())
-	// rootCmd.AddCommand(commands.RepoSetupCmd())
-	rootCmd.AddCommand(ServeCmd())
-}
-
-func ServeCmd() *cobra.Command {
-	return &cobra.Command{
-		Use:   "serve",
-		Short: "Start Server",
-		Long:  "Start the server at the port 8080 ",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return runRepoSetup()
-		},
-	}
-}
-
-func runRepoSetup() error {
-	err := server.Start()
-	fmt.Println(err)
-	return err
+	rootCmd.AddCommand(commands.TaskCmd())
+	rootCmd.AddCommand(commands.RepoSetupCmd())
+	rootCmd.AddCommand(commands.ServeCmd())
 }
 
 func Execute() {
