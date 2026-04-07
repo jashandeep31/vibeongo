50fcaf5eb8e4ffc34a197ea1d15710ab39276e72
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-04-07 16:16:42 +0530

    feat: stats sse

--- core/internal/sse/events.go
+++ core/internal/sse/events.go
@@ -1,46 +1,10 @@
-package handlers
+package sse
 
 import (
 	"bytes"
 	"fmt"
 	"io"
-	"log"
-	"net/http"
-	"time"
-
-	"github.com/labstack/echo/v5"
 )
-
-func StatsHandler(c *echo.Context) error {
-	// Headers for the server side events
-	w := c.Response()
-	w.Header().Set("Content-Type", "text/event-stream")
-	w.Header().Set("Cache-Control", "no-cache")
-	w.Header().Set("Connection", "keep-alive")
-	fmt.Println("hi stats are working")
-
-	ticker := time.NewTicker(1 * time.Second)
-	defer ticker.Stop()
-	count := uint64(0)
-	for {
-		select {
-		case <-c.Request().Context().Done():
-			log.Printf("SSE client disconnected, ip: %v", c.RealIP())
-			return nil
-		case <-ticker.C:
-			count++
-			event := Event{
-				Data: []byte(fmt.Sprintf("count: %d, time: %s\n\n", count, time.Now().Format(time.RFC3339Nano))),
-			}
-			if err := event.MarshalTo(w); err != nil {
-				return err
-			}
-			if err := http.NewResponseController(w).Flush(); err != nil {
-				return err
-			}
-		}
-	}
-}
 
 // Event represents Server-Sent Event.
 // SSE explanation: https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#event_stream_format
