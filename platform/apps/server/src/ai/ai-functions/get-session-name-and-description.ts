7163206b734eaa56491e17e6ac022e8f567f0342
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-04-09 14:52:53 +0530

    feat: project session resume , session name ,description

---
+++ platform/apps/server/src/ai/ai-functions/get-session-name-and-description.ts
@@ -0,0 +1,33 @@
+import { Type } from "@google/genai";
+import { aiModels, googleAiClient } from "../config.js";
+import { prompts } from "../prompts/index.js";
+
+export const getSessionNameAndDescription = async (content: string) => {
+  const res = await googleAiClient.models.generateContent({
+    model: aiModels["google-lite-model"].name,
+    config: {
+      systemInstruction: prompts.getSessionNameAndDescription.systemPrompt(),
+      responseMimeType: "application/json",
+      responseSchema: {
+        type: Type.OBJECT,
+        required: ["name", "description"],
+        properties: {
+          name: {
+            type: Type.STRING,
+          },
+          description: {
+            type: Type.STRING,
+          },
+        },
+      },
+    },
+    contents: [{ role: "user", parts: [{ text: content }] }],
+  });
+
+  const parsedResponse = JSON.parse(res.text || "");
+
+  return parsedResponse as {
+    name: string;
+    description: string;
+  };
+};
