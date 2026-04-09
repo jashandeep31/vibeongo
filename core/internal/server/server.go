08c01424ee89de7b2590eed7a03d2b53d8728630
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-04-09 21:26:24 +0530

    feat: older files

--- core/internal/server/server.go
+++ core/internal/server/server.go
@@ -1,21 +1,11 @@
 package server
 
 import (
-	"embed"
-
-	// _ "github.com/golang-migrate/migrate/v4/database/sqlite"
-	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jashandeep31/vibeongo/core/internal/config"
 	"github.com/jashandeep31/vibeongo/core/internal/routes"
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
-	_ "modernc.org/sqlite"
 )
-
-var migrationsFS embed.FS
-
-// Start
 
 // Start starts the application.
 func Start() error {
