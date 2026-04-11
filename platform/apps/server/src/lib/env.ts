ba9d005ac8bd1261a52754a6fc34b71a0ab2c992
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-04-11 11:20:59 +0530

    feat: githbub auth redirect url fix

--- platform/apps/server/src/lib/env.ts
+++ platform/apps/server/src/lib/env.ts
@@ -9,14 +9,12 @@ const envSchema = z.object({
   NODE_ENV: z
     .enum(["development", "production", "test"])
     .default("development"),
+  BACKEND_URL: z.string(),
   AWS_EC2_ACCESS_KEY_ID: z.string(),
   AWS_EC2_ACCESS_KEY_SECRET: z.string(),
   SSH_KEY: z.string(),
   DATABASE_URL: z.string(),
-  GOOGLE_AUTH_CLIENT_ID: z.string(),
-  GOOGLE_AUTH_SECRET_KEY: z.string(),
   ALLOWED_ORIGINS: z.string().transform((val) => val.split(",")),
-  GOOGLE_AUTH_REDIRECT_URI: z.string(),
   GITHUB_CLIENT_ID: z.string(),
   GITHUB_CLIENT_SECRET: z.string(),
   JWT_SECRET: z.string(),
