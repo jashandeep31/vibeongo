df47903041418e7f3c4c840da1d50345fc4a296b
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-04-12 14:32:36 +0530

    chore: refactor with AI as the go idiomatic approach

--- core/cmd/proxy/main.go
+++ core/cmd/proxy/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"log"
+
 	"github.com/jashandeep31/vibeongo/core/internal/server"
+	"github.com/jashandeep31/vibeongo/core/internal/store"
 )
 
 func main() {
-	server.ProxyServerStart()
+	proxyStore := store.NewProxyManager()
+	srv := server.NewProxyServer(proxyStore)
+
+	log.Println("Starting proxy server on :5000")
+	if err := srv.Start(":5000"); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
