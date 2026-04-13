9f658de527a7b40b2421acddca13b2f0f02b09ff
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-04-13 16:23:10 +0530

    feat: db change for domains

---
+++ platform/packages/database/src/schemas/proxy-domains.ts
@@ -0,0 +1,45 @@
+import {
+  pgTable,
+  varchar,
+  uuid,
+  timestamp,
+  boolean,
+  uniqueIndex,
+} from "drizzle-orm/pg-core";
+import { projects } from "./projects.js";
+
+export const proxyDomains = pgTable(
+  "proxy_domains",
+  {
+    id: uuid("id").defaultRandom().primaryKey(),
+
+    // domain full like xyz.vibeongo.one
+    domain: varchar("domain").notNull().unique(),
+
+    // instance based redirecting
+    target_host: varchar().notNull(),
+    target_port: varchar().notNull(),
+    allow_any: boolean().notNull().default(false),
+
+    project_id: uuid().references(() => projects.id, { onDelete: "cascade" }),
+
+    created_at: timestamp().defaultNow(),
+    updated_at: timestamp().defaultNow(),
+  },
+  (t) => [uniqueIndex("domain_idx").on(t.domain)],
+);
+
+export const allowedIps = pgTable("allowed_ips", {
+  id: uuid("id").defaultRandom().primaryKey(),
+
+  proxy_domain_id: uuid()
+    .references(() => proxyDomains.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+
+  ip: varchar("ip").notNull(),
+
+  created_at: timestamp().defaultNow(),
+  updated_at: timestamp().defaultNow(),
+});
