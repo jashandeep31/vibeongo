73b5625dce3b0bdb0ca64d2ea1376dd425d86f87
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-05-25 15:57:41 +0530

    feat: root go removed

--- core/cmd/vibeongo/main.go
+++ core/cmd/vibeongo/main.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "vibeongo",
+	Short: "Bootstrap and maintenance commands",
+	Run: func(cmd *cobra.Command, args []string) {
+		version := "0.0.2"
+		fmt.Println(version)
+		fmt.Println("Please use a subcommand like: init-workspace, init-repos, run-tasks, serve")
+	},
+}
+
+func init() {
+	// update the vibeongo
+	rootCmd.AddCommand(UpdateCmd())
+	// setup the vps and config the things
+	rootCmd.AddCommand(VpsSetupCmd())
+	// start the echo server
+	rootCmd.AddCommand(ServeCmd())
+	// setup the repo like insatlling the dependencies
+	rootCmd.AddCommand(RepoSetupCmd())
+	// run the tasks in the opencode
+	rootCmd.AddCommand(TaskCmd())
+	// getting config
+	rootCmd.AddCommand(GetconfigCmd())
+	// setting up hte session as per the overview file
+	rootCmd.AddCommand(InitializeSessionFromOverviewCmd())
+	// Updating the overview
+	rootCmd.AddCommand(UpdateSessionFromOverviewCmd())
+	// terminate the instance
+	rootCmd.AddCommand(TerminateInstanceCmd())
+}
+
 func main() {
-	Execute()
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
