888bea463b74e0ba2b1ac503271e168095a9ede5
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-05-26 14:01:19 +0530

    feat: session message working

--- core/internal/ws/ws_functions/storehandler.go
+++ core/internal/ws/ws_functions/storehandler.go
@@ -2,14 +2,10 @@ package wsfunctions
 
 import (
 	"context"
-	"fmt"
 	"sync"
 
 	"github.com/gorilla/websocket"
 )
 
-func StoreWsHandler(ctx context.Context, c *websocket.Conn, writeMu *sync.Mutex) {
-	var data interface{}
-	c.ReadJSON(&data)
-	fmt.Println(data)
+func StoreWsHandler(ctx context.Context, c *websocket.Conn, writeMu *sync.Mutex, msg []byte) {
 }
