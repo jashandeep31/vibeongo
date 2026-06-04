a025f46818cdbb6c0fd519a859b58c6f073c5dc6
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-06-04 10:11:03 +0530

    chore: single query to db for github repo

--- platform/apps/server/src/controllers/github-repo/work-on-pullrequest.ts
+++ platform/apps/server/src/controllers/github-repo/work-on-pullrequest.ts
@@ -2,8 +2,6 @@ import { Request, Response } from "express";
 import { catchAsync } from "../../lib/catch-async.js";
 import { AppError } from "../../lib/app-error.js";
 import z from "zod";
-import { db, githubRepos, eq, and } from "@repo/db";
-import { getPullRequestDetailByPullNumber } from "../../github-app-functions/get-issue-or-pull-request-detail-by-number.js";
 import { pullRequestOpenedHandler } from "../../services/github/pull-request-handler.js";
 
 export const workOnPullRequestByPrNumber = catchAsync(
@@ -18,21 +16,9 @@ export const workOnPullRequestByPrNumber = catchAsync(
       })
       .parse(req.params);
 
-    const [githubRepo] = await db
-      .select()
-      .from(githubRepos)
-      .where(and(eq(githubRepos.id, id), eq(githubRepos.user_id, user.id)));
-
-    if (!githubRepo) throw new AppError("Repo not found", 404);
-
-    const pr = await getPullRequestDetailByPullNumber({
-      installation_id: githubRepo.installation_id,
-      full_repo_name: githubRepo.full_name,
-      pull_number: prNumber,
-    });
     const instance = await pullRequestOpenedHandler({
-      gitRepoId: githubRepo.id,
-      pr,
+      gitRepoId: id,
+      prNumber,
     });
 
     if (!instance) throw new AppError("Something went wrong", 500);
