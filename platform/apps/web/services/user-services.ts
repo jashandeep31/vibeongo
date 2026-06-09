12aebdc8635c88a1f8aded57e48cec6e990a08b7
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-06-09 07:58:38 +0530

    feat: user sidebar

--- platform/apps/web/services/user-services.ts
+++ platform/apps/web/services/user-services.ts
@@ -4,6 +4,9 @@ import axios from "axios";
 export const getUserMetadata = async (): Promise<{
   id: string;
   balance: number;
+  username: string;
+  firstName: string;
+  lastName: string | null;
 }> => {
   const res = await axios.get(`${BACKEND_URL}/api/v1/users/metadata`, {
     withCredentials: true,
