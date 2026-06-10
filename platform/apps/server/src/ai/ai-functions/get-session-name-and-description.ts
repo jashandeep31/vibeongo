f81df04bbaee888db7ce914ec303b91560a8accb
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-06-10 12:42:09 +0530

    feat: naming fix from google

--- platform/apps/server/src/ai/ai-functions/get-session-name-and-description.ts
+++ platform/apps/server/src/ai/ai-functions/get-session-name-and-description.ts
@@ -3,31 +3,38 @@ import { aiModels, googleAiClient } from "../config.js";
 import { prompts } from "../prompts/index.js";
 
 export const getSessionNameAndDescription = async (content: string) => {
-  const res = await googleAiClient.models.generateContent({
-    model: aiModels["google-lite-model"].name,
-    config: {
-      systemInstruction: prompts.getSessionNameAndDescription.systemPrompt(),
-      responseMimeType: "application/json",
-      responseSchema: {
-        type: Type.OBJECT,
-        required: ["name", "description"],
-        properties: {
-          name: {
-            type: Type.STRING,
-          },
-          description: {
-            type: Type.STRING,
+  try {
+    const res = await googleAiClient.models.generateContent({
+      model: aiModels["google-lite-model"].name,
+      config: {
+        systemInstruction: prompts.getSessionNameAndDescription.systemPrompt(),
+        responseMimeType: "application/json",
+        responseSchema: {
+          type: Type.OBJECT,
+          required: ["name", "description"],
+          properties: {
+            name: {
+              type: Type.STRING,
+            },
+            description: {
+              type: Type.STRING,
+            },
           },
         },
       },
-    },
-    contents: [{ role: "user", parts: [{ text: content }] }],
-  });
+      contents: [{ role: "user", parts: [{ text: content }] }],
+    });
 
-  const parsedResponse = JSON.parse(res.text || "");
+    const parsedResponse = JSON.parse(res.text || "");
 
-  return parsedResponse as {
-    name: string;
-    description: string;
-  };
+    return parsedResponse as {
+      name: string;
+      description: string;
+    };
+  } catch {
+    return {
+      name: "Unkown",
+      description: content,
+    };
+  }
 };
