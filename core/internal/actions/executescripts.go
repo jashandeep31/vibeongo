8d658f424fbce5b11134b49f763b03eb1ef3f17c
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-06-12 15:31:14 +0530

    feat: vibeongo final scrip improvements

--- core/internal/actions/executescripts.go
+++ core/internal/actions/executescripts.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"regexp"
 
 	"github.com/jashandeep31/vibeongo/core/internal/config"
 	"github.com/jashandeep31/vibeongo/core/internal/utils"
@@ -54,31 +55,74 @@ func ExecuteFinalScript() error {
 		return err
 	}
 
-	tempScriptFile, err := os.CreateTemp("", "temp.sh")
-	if err != nil {
-		return err
-	}
-	defer tempScriptFile.Close()
-	defer os.Remove(tempScriptFile.Name())
+	re := regexp.MustCompile(`(?m)^---\s*$`)
+	parts := re.Split(cfg.FinalScript, -1)
 
-	script := `#!/usr/bin/env bash
-	source /home/ubuntu/.bashrc
-	`
-	script = script + cfg.FinalScript
+	ensureSession := exec.Command("tmux", "new-session", "-d", "-s", "dev")
+	ensureSession.Run()
 
-	path := "/home/ubuntu/code"
+	for i, part := range parts {
+		fmt.Println("Running the part")
+		fmt.Println(part)
+		tempScriptFile, err := os.CreateTemp("", "temp-*.sh")
+		if err != nil {
+			return err
+		}
 
-	tempScriptFile.Write([]byte(script))
-	cmd := utils.ExecCommand(utils.SudoShellScriptFile, tempScriptFile.Name())
-	cmd.Dir = path
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+		if _, err := tempScriptFile.Write([]byte(part)); err != nil {
+			tempScriptFile.Close()
+			os.Remove(tempScriptFile.Name())
+			return err
+		}
 
-	err = cmd.Run()
-	if err != nil {
-		return err
+		if err := tempScriptFile.Close(); err != nil {
+			os.Remove(tempScriptFile.Name())
+			return err
+		}
+
+		if err := os.Chmod(tempScriptFile.Name(), 0755); err != nil {
+			os.Remove(tempScriptFile.Name())
+			return err
+		}
+		cmd := exec.Command("tmux", "new-window", "-t", "dev", "-n", fmt.Sprintf("task-%d", i), "bash", tempScriptFile.Name())
+		if err := cmd.Run(); err != nil {
+			os.Remove(tempScriptFile.Name())
+			return err
+		}
+
+		// os.Remove(tempScriptFile.Name())
+		// deleting this causes the issue
+		// defer tempScriptFile.Close()
+		// defer os.Remove(tempScriptFile.Name())
 	}
 
 	return nil
+	// tempScriptFile, err := os.CreateTemp("", "temp.sh")
+	// if err != nil {
+	// 	return err
+	// }
+	//
+	// defer tempScriptFile.Close()
+	// defer os.Remove(tempScriptFile.Name())
+	//
+	// script := `#!/usr/bin/env bash
+	// source /home/ubuntu/.bashrc
+	// `
+	// script = script + cfg.FinalScript
+	//
+	// path := "/home/ubuntu/code"
+	//
+	// tempScriptFile.Write([]byte(script))
+	// cmd := utils.ExecCommand(utils.SudoShellScriptFile, tempScriptFile.Name())
+	// cmd.Dir = path
+	// cmd.Stdin = os.Stdin
+	// cmd.Stdout = os.Stdout
+	// cmd.Stderr = os.Stderr
+	//
+	// err = cmd.Run()
+	// if err != nil {
+	// 	return err
+	// }
+	//
+	// return nil
 }
