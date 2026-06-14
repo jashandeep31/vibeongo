91352365cbac27eadbc5a8fae9f8625150018744
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-06-14 09:54:41 +0530

    feat: auth fix

--- platform/apps/web/hooks/use-user.ts
+++ platform/apps/web/hooks/use-user.ts
@@ -1,8 +1,16 @@
 import { getUserMetadata } from "@/services/user-services";
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 export const useUserMetadata = () =>
   useQuery({
     queryKey: ["user-metadata"],
     queryFn: getUserMetadata,
+    retry: (failureCount, error) => {
+      if (axios.isAxiosError(error) && error.response?.status === 401) {
+        return false;
+      }
+
+      return failureCount < 3;
+    },
   });
