e100d20c7f06219c5cee616324250fa748eb220f
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-06-16 10:12:00 +0530

    feat: restart final script route

--- core/internal/routes/routes.go
+++ core/internal/routes/routes.go
@@ -11,6 +11,7 @@ func Register(e *echo.Echo) {
 	e.GET("/ws", ws.WebSocket)
 	e.GET("/ufw", handlers.GetAllowedPorts)
 	e.POST("/reboot", handlers.RebootServer)
+	e.POST("/restart-final-script", handlers.RestartFinalScriptHandler)
 
 	e.GET("/opencode/web/status", handlers.OpenCodeWebStatus)
 	e.POST("/opencode/web", handlers.OpenCodeWeb)
