9778063c64b5ca9993364a0bf3f81c910151b2c2
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-06-19 16:21:37 +0530

    feat: encryption if project files

--- platform/apps/server/src/controllers/project/project-files.ts
+++ platform/apps/server/src/controllers/project/project-files.ts
@@ -6,11 +6,20 @@ import {
   projectFileData,
   projectFiles,
   projects,
+  ConsoleLogWriter,
 } from "@repo/db";
 import { AppError } from "../../lib/app-error.js";
 import { catchAsync } from "../../lib/catch-async.js";
 import { Request, Response } from "express";
 import { z } from "zod";
+import { decryptData, encryptData } from "../../lib/encryption-decryption.js";
+
+type RefinedFile = typeof projectFiles.$inferSelect & {
+  projectFileData?: Pick<
+    typeof projectFileData.$inferSelect,
+    "id" | "version" | "created_at" | "updated_at"
+  > & { content: string };
+};
 
 export const getProjectFiles = catchAsync(
   async (req: Request, res: Response) => {
@@ -42,8 +51,44 @@ export const getProjectFiles = catchAsync(
       )
       .where(and(eq(projects.user_id, user.id), eq(projects.id, id)));
 
+    const refinedFiles: Map<string, RefinedFile> = new Map();
+
+    for (const item of rows) {
+      console.log(item);
+      if (item.projectFiles) {
+        if (!refinedFiles.has(item.projectFiles.id)) {
+          refinedFiles.set(item.projectFiles.id, {
+            ...item.projectFiles,
+          });
+        }
+      }
+      if (item.projectFileData) {
+        const projectFile = item.projectFileData;
+        const selectedItem = refinedFiles.get(
+          item.projectFileData.project_file_id,
+        );
+        if (!selectedItem) continue;
+        console.log("iam runngin");
+
+        refinedFiles.set(selectedItem.id, {
+          ...selectedItem,
+          projectFileData: {
+            id: projectFile.id,
+            content: decryptData({
+              encrypted: projectFile.encrypted_content,
+              iv: projectFile.iv,
+              tag: projectFile.tag,
+            }),
+            created_at: projectFile.created_at,
+            updated_at: projectFile.updated_at,
+            version: projectFile.version,
+          },
+        });
+      }
+    }
+
     res.status(200).json({
-      data: rows,
+      data: [...refinedFiles.values()],
     });
   },
 );
@@ -83,9 +128,14 @@ export const createProjectFile = catchAsync(
 
       if (!projectfile) throw new AppError("project file not created", 400);
 
+      const enc = encryptData(content);
       await tx.insert(projectFileData).values({
         project_file_id: projectfile.id,
-        content: content,
+
+        encrypted_content: enc.encryptedData,
+        iv: enc.iv,
+        tag: enc.tag,
+
         version: 1,
       });
     });
@@ -135,6 +185,7 @@ export const updateProjectFile = catchAsync(
             and(eq(projectFiles.id, fileId), eq(projectFiles.project_id, id)),
           );
       }
+      // const enc = encryptData()
 
       if (content !== undefined) {
         const [latestData] = await tx
@@ -144,12 +195,26 @@ export const updateProjectFile = catchAsync(
           .orderBy(desc(projectFileData.version))
           .limit(1);
 
-        if (!latestData || latestData.content !== content) {
+        let decryptedPrevContent = null;
+
+        if (latestData) {
+          decryptedPrevContent = decryptData({
+            iv: latestData.iv,
+            tag: latestData.tag,
+            encrypted: latestData?.encrypted_content,
+          });
+        }
+
+        if (decryptedPrevContent !== content) {
+          const enc = encryptData(content);
           const newVersion = latestData ? latestData.version + 1 : 1;
           await tx.insert(projectFileData).values({
             project_file_id: fileId,
-            content: content,
             version: newVersion,
+
+            encrypted_content: enc.encryptedData,
+            iv: enc.iv,
+            tag: enc.tag,
           });
         }
       }
