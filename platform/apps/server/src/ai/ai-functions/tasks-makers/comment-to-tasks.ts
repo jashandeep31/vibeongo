f579e46a3c218ffe8be7c79029d8cedfca587030
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-06-19 18:46:26 +0530

    feat: env files handlings

---
+++ platform/apps/server/src/ai/ai-functions/tasks-makers/comment-to-tasks.ts
@@ -0,0 +1,4 @@
+/**
+ * Takes the comment from the issue or the pr under stand the context then generates the tasks as per it
+ */
+export const commentToTasks = async (comment: string, url: string) => {};
