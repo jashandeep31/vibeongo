37a54f18d3137a2c3ff7b06a277ac75012f9ee36
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-06-22 12:47:55 +0530

    feat: ami from the database

--- platform/apps/server/src/aws/configs/ec2-region-image-config.ts
+++ platform/apps/server/src/aws/configs/ec2-region-image-config.ts
@@ -1,17 +1,17 @@
-import { awsSupportedRegions } from "./aws-supported-regions-configs.js";
+// import { awsSupportedRegions } from "./aws-supported-regions-configs.js";
 
-export const ec2RegionImageIds: {
-  region: (typeof awsSupportedRegions)[number];
-  linuxImageId: string;
-}[] = [
-  {
-    region: "ap-south-1",
-    linuxImageId: "ami-0984c70bbe282f599",
-  },
-  { region: "us-east-1", linuxImageId: "ami-021dce13887b692e1" },
-] as const;
-
-// export const getAWSLinuxAmis = async () => {
+// export const ec2RegionImageIds: {
+//   region: (typeof awsSupportedRegions)[number];
+//   linuxImageId: string;
+// }[] = [
+//   {
+//     region: "ap-south-1",
+//     linuxImageId: "ami-0984c70bbe282f599",
+//   },
+//   { region: "us-east-1", linuxImageId: "ami-021dce13887b692e1" },
+// ] as const;
+//
+// // export const getAWSLinuxAmis = async () => {
 //   console.log(`amis is running `);
 //   const command = new DescribeImagesCommand({
 //     Owners: [],
