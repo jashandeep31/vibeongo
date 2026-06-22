1aaa79c4e22ec2f99c5cd2c45060bc3b262155a2
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-06-22 12:47:55 +0530

    feat: ami from the database

--- platform/apps/server/src/aws/services/create-ec2-instance.ts
+++ platform/apps/server/src/aws/services/create-ec2-instance.ts
@@ -1,8 +1,8 @@
 import { _InstanceType, RunInstancesCommand } from "@aws-sdk/client-ec2";
 import { getEc2Client } from "../ec2-client.js";
 import { awsSupportedRegions } from "../configs/aws-supported-regions-configs.js";
-import { ec2RegionImageIds } from "../configs/ec2-region-image-config.js";
-import { env } from "../../lib/env.js";
+import { db, eq, instanceRegions } from "@repo/db";
+import { AppError } from "../../lib/app-error.js";
 
 /**
  * Create a ec2 instance depending upon the user requirnments
@@ -16,16 +16,14 @@ export const createEc2Instance = async ({
   instanceType: string;
   userData: string;
 }) => {
-  const imageConfig = ec2RegionImageIds.find((item) => item.region === region);
-  if (!imageConfig)
-    throw new Error("Regions isn't suppported yet for ec2 deployment");
-
-  if (!Object.values(_InstanceType).includes(instanceType as _InstanceType)) {
-    throw new Error(`Unsupported EC2 instance type: ${instanceType}`);
-  }
+  const [regionRow] = await db
+    .select()
+    .from(instanceRegions)
+    .where(eq(instanceRegions.slug, region));
+  if (!regionRow) throw new AppError("Not a valid region ", 404);
 
   const command = new RunInstancesCommand({
-    ImageId: imageConfig.linuxImageId,
+    ImageId: regionRow.ami,
     InstanceType: instanceType as _InstanceType,
     MinCount: 1,
     MaxCount: 1,
