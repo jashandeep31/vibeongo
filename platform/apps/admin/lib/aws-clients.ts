0417c284ed6501491a782ba0b081ee2854ea9808
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-06-22 16:36:03 +0530

    feat: env file removal

--- platform/apps/admin/lib/aws-clients.ts
+++ platform/apps/admin/lib/aws-clients.ts
@@ -1,5 +1,4 @@
 import { EC2Client } from "@aws-sdk/client-ec2";
-import { env } from "./env";
 import { ImagebuilderClient } from "@aws-sdk/client-imagebuilder";
 
 export const validRegions = ["us-east-1", "ap-south-1"] as const;
@@ -9,8 +8,8 @@ export const getEc2Client = (region: ValidRegion) => {
   return new EC2Client({
     region: region,
     credentials: {
-      accessKeyId: env.AWS_EC2_ACCESS_KEY_ID,
-      secretAccessKey: env.AWS_EC2_ACCESS_KEY_SECRET,
+      accessKeyId: process.env.AWS_EC2_ACCESS_KEY_ID ?? "",
+      secretAccessKey: process.env.AWS_EC2_ACCESS_KEY_SECRET ?? "",
     },
   });
 };
@@ -19,8 +18,8 @@ export const getImageBuilderClient = (region: ValidRegion) => {
   return new ImagebuilderClient({
     region: region,
     credentials: {
-      accessKeyId: env.AWS_EC2_ACCESS_KEY_ID,
-      secretAccessKey: env.AWS_EC2_ACCESS_KEY_SECRET,
+      accessKeyId: process.env.AWS_EC2_ACCESS_KEY_ID ?? "",
+      secretAccessKey: process.env.AWS_EC2_ACCESS_KEY_SECRET ?? "",
     },
   });
 };
