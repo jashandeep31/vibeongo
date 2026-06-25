2eaeb5bcfdaa286cbc7790c1525f5ce8cf088058
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-06-25 16:49:42 +0530

    feat: add AI tools for managing GitHub repositories

--- platform/apps/server/src/websocket/handlers/newchat-handler.ts
+++ platform/apps/server/src/websocket/handlers/newchat-handler.ts
@@ -4,6 +4,10 @@ import { WebSocket } from "ws";
 import { AppError } from "../../lib/app-error.js";
 import { generateText, stepCountIs, tool } from "ai";
 import { projectValidatorForAIInput } from "@repo/shared";
+import {
+  createNewGithubRepo,
+  getUserReposAITool,
+} from "../../ai/ai-tools/index.js";
 // import {v4 as } from "u  "
 
 export const newChatHandler = async (socket: WebSocket, eventData: unknown) => {
@@ -42,7 +46,7 @@ export const newChatHandler = async (socket: WebSocket, eventData: unknown) => {
   // });
 
   console.log(`we are working `);
-  const answer = await aiWork(parsedData.question);
+  const answer = await aiWork(parsedData.question, socket.userId);
   // await db.insert(chatAnswer).values({
   //   question_id: chatQuestion.id,
   //   answer: answer,
@@ -72,15 +76,17 @@ const getCurrentConfig = tool({
   },
 });
 
-const aiWork = async (question: string) => {
+const aiWork = async (question: string, userId: string) => {
   const result = await generateText({
     model: "openai/gpt-5-nano",
     system:
       "You are a expert vibeongo project config maker. You can ask the question regading that as needed and can use the internal tools for it. You have ask user to for each tthing as the user want so do me your own Your motive is compelte the config you can ask user next queistons  , if config is not rpooerp you can getprev config fomthe getCurrentConfig toll and after adding new thigns those areginven i nte thsi pronpt call updateConfig then asked the user  quesions about missign thisns",
     tools: {
       // weatherTool,
+      getUserReposAITool: getUserReposAITool(userId),
       getCurrentConfig,
       updateConfig,
+      createNewGithubRepo: createNewGithubRepo(userId),
     },
     stopWhen: stepCountIs(2),
     prompt: question,
