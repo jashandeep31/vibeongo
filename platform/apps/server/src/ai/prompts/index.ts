ce1087b89da940771c064ef82f7fd0e6a984e6f8
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-06-27 00:07:43 +0530

    feat: txt files based propmpts

--- platform/apps/server/src/ai/prompts/index.ts
+++ platform/apps/server/src/ai/prompts/index.ts
@@ -1,6 +1,10 @@
+import { cwd } from "node:process";
 import { createProjetSystemPrompt } from "./create-project-prompt-system.js";
 import { getRefinedTaskFromUserIssuesCommentSystemPrompt } from "./get-refined-task-from-user-issues-comment-system-prompt.js";
 import { getSessionNameAndDescriptionSystemPrompt } from "./get-session-name-and-description-system-prompt.js";
+import path from "node:path";
+
+export const PROMPT_TEXT_FILES_FOLDER_PATH = path.resolve(cwd(), "prompts");
 
 export const prompts = {
   getRefinedTaskFromUserIssuesComment: {
