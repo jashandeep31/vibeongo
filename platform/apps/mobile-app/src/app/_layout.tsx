850719efd0197da440b9c56fef27981f01eb0989
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-08-12 17:45:52 +0530

    feat: basic cleanup

--- platform/apps/mobile-app/src/app/_layout.tsx
+++ platform/apps/mobile-app/src/app/_layout.tsx
@@ -1,18 +1,20 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useColorScheme } from 'react-native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
+import { useTheme } from '@/hooks/use-theme';
 
-SplashScreen.preventAutoHideAsync();
+export default function RootLayout() {
+  const theme = useTheme();
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <>
+      <Stack
+        screenOptions={{
+          contentStyle: { backgroundColor: theme.background },
+          headerShown: false,
+        }}
+      />
+      <StatusBar style="auto" />
+    </>
   );
 }
