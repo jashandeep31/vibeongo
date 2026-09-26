1924fa40be08d2e733853d52be501a7ed1b547f7
Jashandeep Singh <jashandeep1659@gmail.com>
2026-09-26 13:57:36 +0530

    chore: droped cli apps. will move it to remote later

--- platform/apps/cli/src/commands/wallet.ts
+++
@@ -1,8 +0,0 @@
-import { Effect } from "effect";
-import { getWalletBalance } from "../services/wallet.js";
-
-export function wallet() {
-  return Effect.flatMap(getWalletBalance(), (balance) =>
-    Effect.sync(() => console.log(balance)),
-  );
-}
--- platform/apps/cli/src/lib/api-client.ts
+++
@@ -1,6 +0,0 @@
-import { MobileClient } from "@repo/api-client";
-import { BACKEND_URL } from "../constants/config.js";
-
-export function createApiClient(token: string) {
-  return new MobileClient(BACKEND_URL, token);
-}
