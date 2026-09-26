a1be2ab0de7448dc0ba87f3c507fd75ab14c1dac
Jashandeep Singh <jashandeep1659@gmail.com>
2026-09-26 11:03:02 +0530

    feat: commander setup

--- platform/apps/cli/src/index.ts
+++ platform/apps/cli/src/index.ts
@@ -1,1 +1,8 @@
-console.log("Vibeongo CLI");
+import { Command } from "commander";
+
+const program = new Command();
+program.name("vibeongo").version("Verion 0.0.1", "--version");
+program.parse();
+if (process.argv.length === 2) {
+  console.log("Vibeongo CLI");
+}
